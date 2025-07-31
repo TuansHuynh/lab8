52bd9ea5f1058cb2fe3398ed9efb966fd9a45c96
TuansHuynh <nkoc.nho.17.04@gmail.com>
2025-07-31 14:08:17 +0700

    create router

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+
+export default function Layout() {
+    return (
+        <div>
+
+            <div>
+                <Header />
+            </div>
+            <div>
+                <Outlet />
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </div>
+    );
+}
--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -3,7 +3,7 @@ import './home.css'
 export default function Home (){
     return(
         <div>
-
+            Home
         </div>
     )
 }

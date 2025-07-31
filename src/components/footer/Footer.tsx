7363246ba76392d36d3888e46aa2733ecdef833b
TuansHuynh <nkoc.nho.17.04@gmail.com>
2025-07-31 15:39:48 +0700

    merge movie

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
         <footer className="footer">
             <div className="footer-container">
                 <p>&copy; 2025 YourCompany. All rights reserved.</p>
-                <div className="footer-links">
+                {/* <div className="footer-links">
                     <a href="#">About</a>
                     <a href="#">Contact</a>
                     <a href="#">Privacy Policy</a>
-                </div>
+                </div> */}
             </div>
         </footer>
     );

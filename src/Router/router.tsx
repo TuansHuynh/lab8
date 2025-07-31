2fcb5df3a13dacef18cf3c1b0439341a5b83a308
TuansHuynh <nkoc.nho.17.04@gmail.com>
2025-07-31 15:15:05 +0700

    complete header and footer

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../page/home/Home";
+import About from "../page/about/About";
+import MovieDetail from "../page/movie/MovieDetail";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
+            },
+            {
+                path: '/about',
+                element: <About/>
+            },
+            {
+                path: 'detail',
+                element: <MovieDetail/>
             }
         ]
     }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,17 @@
-export default function Header(){
-    return(
-        <div>
-            Header
-        </div>
-    )
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">MyApp</div>
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/detail">Movie Detail</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </div>
+    </header>
+  );
 }
